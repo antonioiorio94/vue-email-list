e6a812edfd077b1b152d5c49365fbbf6380075c4
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-04 17:25:03 +0200

    aggiunta ciclo for e visualizzazione nel dom. Fine esercizio

--- main.js
+++ main.js
@@ -3,17 +3,22 @@ console.log("Js OK!");
 const app = new Vue({
   el: "#app",
   data: {
-    randomEmail: "",
+    randomEmails: [],
   },
   mounted() {
     // Aggiunta Axios
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((response) => {
-        const result = response.data;
-        console.log(result);
-        //il valore di response viene assegnato a randomEmail
-        this.randomEmail = result.response;
-      });
+    //ciclo per generare 10 email
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          const result = response.data;
+          //console.log(result);
+          //il valore di response viene assegnato a randomEmail
+          this.randomEmail = result.response;
+          this.randomEmails.push(this.randomEmail);
+        });
+    }
+    console.log(this.randomEmails);
   },
 });
