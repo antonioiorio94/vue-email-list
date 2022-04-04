887510fb8f10f382b98963f9ce3b549d06807cc5
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-04 16:50:04 +0200

    aggiunta Axios e visaulizzazione della prima mail nel DOM

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("Js OK!");
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    randomEmail: "",
+  },
+  mounted() {
+    // Aggiunta Axios
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        const result = response.data;
+        console.log(result);
+        //il valore di response viene assegnato a randomEmail
+        this.randomEmail = result.response;
+      });
+  },
+});
